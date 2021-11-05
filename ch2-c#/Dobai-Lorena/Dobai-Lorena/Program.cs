df26a857ce0bd5883c7c0ba46f546065e2b546fc
lorenadobai <93092056+lorenadobai@users.noreply.github.com>
2021-11-05 14:53:22 +0200

    added

---
+++ ch2-c#/Dobai-Lorena/Dobai-Lorena/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dobai_Lorena //used for organization
+{
+    class Program //has members(method)
+    {
+        static void Main(string[] args) //a method-contains a block of code
+        {
+            Program myProgram = new Program();
+            myProgram.Print();
+            //Console.WriteLine(args[0]);
+            var name= Console.ReadLine();
+            // Console.WriteLine("hello "+name+"!");
+            Console.WriteLine($"Hello {name}!");
+            Console.WriteLine("yo");
+            //int x = 5;
+            // int X = 50;
+            //var y = "hello";
+            string x = "caleb";
+            Console.WriteLine(x+" is weird.");
+        }
+        void Print()
+        {
+            Console.WriteLine("hey");
+            Console.WriteLine("Hello World!"); //statement
+            Console.WriteLine("Best friend!");
+        }
+    }
+}
+//create the method->invoke the method
+//namespace->classes->members->method->statements
+//variable- stores some value
+//identifier- what we call the variable
+//expression- evaluates to a value
+//operator-it workd on operands
+//operand-the things that the operator works on
