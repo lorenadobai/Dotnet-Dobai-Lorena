e9a7f51772f11f9cfa475e1d45c388ea60f1c49a
Dobai Lorena <lorena_dobai@yahoo.com>
2021-11-05 17:43:08 +0200

    tutorial 14-17

--- ch2-c#/Dobai-Lorena2/Dobai-Lorena2/Program.cs
+++ ch2-c#/Dobai-Lorena2/Dobai-Lorena2/Program.cs
@@ -29,6 +29,46 @@ namespace Dobai_Lorena2
             int x = (int)5.9999999;
             double y = 5;
             Console.WriteLine(x);
+            Console.WriteLine(y);
+            //float f = 5.5f;//when we do not have enough space
+            double d = 5.5;
+            decimal d2 = 10.5M;//precizie,working with money
+            // double t = double.NaN;
+            // Console.WriteLine(double.IsNaN(t));
+            // double t = Math.Pow(d, 2);
+            // Console.WriteLine(t);
+            double Y = 50.5;
+            double X = 300;
+            Console.WriteLine(Math.Round(Y,0,MidpointRounding.AwayFromZero));
+            Console.WriteLine(Math.Floor(Y));
+            Console.WriteLine(Math.Ceiling(Y));
+            Console.WriteLine(Math.Truncate(Y));
+            Console.WriteLine(Math.Max(X, Y));
+            Console.WriteLine(Math.Min(X, Y));
+            Console.WriteLine(Math.Abs(-50));//scrie varianta pozitiva
+            Console.WriteLine(Math.Sign(Y));//verifica daca nr este pozitiv sau negativ:-1 nr negativ,1 nr pozitiv, 0 nr este 0
+            char letter = 'a';
+            int number = 'a';
+            char c = (char)977;
+            Console.WriteLine("Hello\tLore");
+            Console.WriteLine(c);
+            string name = "Lore";
+            string name2 = name;
+            //name += " Curry";
+            Console.WriteLine(name);
+            Console.WriteLine(name2);
+            char[] ch = { 'h', 'e', 'l', 'l', 'o' };
+            string greetings = new string(ch);
+            Console.WriteLine(greetings);
+            string message = $"Hello my name is {name}, and I like to say {greetings}";
+            Console.WriteLine(message);
+            Console.WriteLine(name.Length);
+            Console.WriteLine(name.CompareTo("Lore"));
+            Console.WriteLine(name.Remove(1, 2));
+           // Console.WriteLine(name.Trim());
+            string blabla = "hello my name is";
+            string[] words=blabla.Split(' ');
+            Console.WriteLine(words[1]);
         }
         /*static void Test(int[] a)
         {
