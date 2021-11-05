723a2b458096cca41875847a990cfdee4ac99a83
Dobai Lorena <lorena_dobai@yahoo.com>
2021-11-05 16:05:03 +0200

    from tutorial 11

---
+++ ch2-c#/Dobai-Lorena2/Dobai-Lorena2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Dobai_Lorena2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -5;
+            int b = a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            b = 10;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            int[] A = { -5 };
+            int[] B = A;
+            Console.WriteLine(A[0]);
+            Console.WriteLine(B[0]);
+            B[0] = 10;
+            Console.WriteLine(A[0]);
+            Console.WriteLine(B[0]);
+            // Test(A);
+            //Console.WriteLine(A[0]);
+            // Test(a);
+            // Console.WriteLine(a);
+            //Int32 x = 5;
+            //string x = "caleb";
+        }
+        /*static void Test(int[] a)
+        {
+            a[0] = 100;
+        }*/
+       /* static void Test(int x)
+        {
+            x = 100;
+        }*/
+    }
+}
